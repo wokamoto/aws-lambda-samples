2b2a4627f483f42e91e9a92816d3df4732938fc1
wokamoto <wokamoto1973@gmail.com>
2015-03-07 21:55:52 +0900

    added typetalk support

--- s3slack/index.js
+++ s3slack/index.js
@@ -1,14 +1,20 @@
-const SLACK_TOKEN = "xxx...xxx";
+const SLACK_TOKEN = "....";
+const TYPETALK_TOKEN = "....";
 
 var req = require('request');
 
 exports.handler = function(event, context) {
     var filename = event.Records[0].s3.object.key;
+    slack(SLACK_TOKEN, "your slack channel id", "Put: "+filename);
+    typetalk(TYPETALK_TOKEN, "your typetalk toppick id", "Put: "+filename);
+};
+
+function slack(token, channel, message) {
     req.post('https://slack.com/api/chat.postMessage')
         .form({
-            token: SLACK_TOKEN,
-            channel: "#s3",
-            text: "Put: "+filename
+            token: token,
+            channel: channel,
+            text: message
         })
         .on('response', function (response) {
           response.on('data', function(data) {
@@ -18,4 +24,19 @@ exports.handler = function(event, context) {
         .on('error', function (err) {
             cotext.done(err, 'Failed post the chat');
         });
-};
+}
+
+function typetalk(token, topic_id, message) {
+    req.post('https://typetalk.in/api/v1/topics/'+topic_id+'?typetalkToken='+token)
+        .form({
+            message: message
+        })
+        .on('response', function (response) {
+          response.on('data', function(data) {
+            context.done(null, data);
+          });
+        })
+        .on('error', function (err) {
+            cotext.done(err, 'Failed post the chat');
+        });
+}
